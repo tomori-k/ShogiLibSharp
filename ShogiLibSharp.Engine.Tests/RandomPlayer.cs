15918a46217e69d7845fe56e052fcd0f8dcd84b1
tomori-k <108829509+tomori-k@users.noreply.github.com>
2022-07-30 17:12:01 +0900

    ランダムプレイヤのモック作成

---
+++ ShogiLibSharp.Engine.Tests/RandomPlayer.cs
@@ -0,0 +1,134 @@
+﻿using ShogiLibSharp.Core;
+using ShogiLibSharp.Engine.Process;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShogiLibSharp.Engine.Tests
+{
+    internal class RandomPlayer : IEngineProcess
+    {
+        private Core.Position? pos = null;
+        private Random rnd = new(0);
+        private TaskCompletionSource? tcsGo = null;
+        private TaskCompletionSource? tcsPonder = null;
+
+        public event Action<string?>? StdOutReceived;
+        public event Action<string?>? StdErrReceived;
+        public event EventHandler Exited;
+
+        public void BeginErrorReadLine() {}
+        public void BeginOutputReadLine() {}
+        public void Dispose() {}
+
+        public void Kill()
+        {
+            Exited?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void SendLine(string message)
+        {
+            Trace.WriteLine($"< {message}");
+            if (message == "usi") Usi();
+            else if (message == "isready") IsReady();
+            else if (message.StartsWith("position")) Position(message);
+            else if (message.StartsWith("go ponder")) GoPonder(message);
+            else if (message.StartsWith("go")) Go(message);
+            else if (message == "quit") Quit();
+            else if (message == "stop") Stop();
+            else if (message == "ponderhit") PonderHit();
+        }
+
+        private void PonderHit()
+        {
+            tcsPonder?.SetResult();
+        }
+
+        private void SendRandomMove()
+        {
+            var moves = Movegen.GenerateMoves(pos);
+            var select = moves[rnd.Next(moves.Count)];
+            pos.DoMove(select);
+            if (pos.IsMated())
+            {
+                StdOutReceived?.Invoke($"bestmove {select.Usi()}");
+            }
+            else
+            {
+                var moves2 = Movegen.GenerateMoves(pos);
+                var ponder = moves2[rnd.Next(moves2.Count)];
+                StdOutReceived?.Invoke($"bestmove {select.Usi()} ponder {ponder.Usi()}");
+            }
+        }
+
+        private async void GoPonder(string message)
+        {
+            tcsPonder = new TaskCompletionSource();
+            await tcsPonder.Task;
+            SendRandomMove();
+        }
+
+        private void Stop()
+        {
+            tcsGo?.SetResult();
+            tcsPonder?.SetResult();
+        }
+
+        private void Quit()
+        {
+            Exited?.Invoke(this, EventArgs.Empty);
+        }
+
+        private async void Go(string message)
+        {
+            tcsGo = new TaskCompletionSource();
+            var byoyomi = int.Parse(message.Split()[6]);
+
+            await Task.WhenAny(Task.Delay(byoyomi), tcsGo.Task);
+
+            SendRandomMove();
+        }
+
+        private void Position(string command)
+        {
+            var sfen = string.Join(' ', command.Split().Skip(2).Take(4));
+            var moves = command
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .SkipWhile(x => x != "moves")
+                .Skip(1)
+                .Select(x => Core.Usi.ParseMove(x));
+            pos = new Core.Position(sfen);
+            foreach (var m in moves)
+            {
+                pos.DoMove(m);
+            }
+        }
+
+        private async void IsReady()
+        {
+            await Task.Delay(10);
+            StdOutReceived?.Invoke("readyok");
+        }
+
+        private void Usi()
+        {
+            StdOutReceived?.Invoke("id name RandomPlayer");
+            StdOutReceived?.Invoke("id author Author0112");
+            StdOutReceived?.Invoke("option name USI_Hash type spin default 1024 min 1 max 33554432");
+            StdOutReceived?.Invoke("option name USI_Ponder type check default false");
+            StdOutReceived?.Invoke("option name NodesLimit type spin default 0 min 0 max 9223372036854775807");
+            StdOutReceived?.Invoke("option name BookEvalLimit type spin default 0 min -99999 max 99999");
+            StdOutReceived?.Invoke("option name SomeFile type filename default <empty>");
+            StdOutReceived?.Invoke("option name BookFile type combo default no_book var no_book var standard_book.db var yaneura_book1.db var yaneura_book2.db var yaneura_book3.db var yaneura_book4.db var user_book1.db var user_book2.db var user_book3.db var book.bin");
+            StdOutReceived?.Invoke("usiok");
+        }
+
+        public bool Start()
+        {
+            return true;
+        }
+    }
+}
--- ShogiLibSharp.Engine.Tests/UsiEngineTests.cs
+++ ShogiLibSharp.Engine.Tests/UsiEngineTests.cs
@@ -19,90 +19,75 @@ namespace ShogiLibSharp.Engine.Tests
         [TestMethod(), Timeout(10000)]
         public async Task UsiEngineTest()
         {
-            var (process, log) = CreateMockProcess();
+            using var engine1 = new UsiEngine(new RandomPlayer());
+            using var engine2 = new UsiEngine(new RandomPlayer());
+
+            //engine1.StdIn += s => Trace.WriteLine($"< {s}");
+            //engine1.StdOut += s => Trace.WriteLine($"  > {s}");
+
+            await Task.WhenAll(engine1.BeginAsync(), engine2.BeginAsync());
+            await Task.WhenAll(engine1.IsReadyAsync(), engine2.IsReadyAsync());
+            engine1.StartNewGame();
+            engine2.StartNewGame();
+
+            var pos = new Position(Position.Hirate);
+            var limits = SearchLimit.Create(TimeSpan.Zero);
+            var (p, o) = (engine1, engine2);
+
+            while (!pos.IsMated()
+                && pos.CheckRepetition() == Repetition.None)
+            {
+                var (bestmove, ponder) = await p.GoAsync(pos, limits);
+                pos.DoMove(bestmove);
+                if (pos.IsLegalMove(ponder))
+                {
+                    pos.DoMove(ponder);
+                    p.GoPonder(pos, limits);
+                    pos.UndoMove();
+                }
+                (p, o) = (o, p);
+            }
+
+            await Task.WhenAll(p.StopPonderAsync(), o.StopPonderAsync());
+
+            if (pos.CheckRepetition() == Repetition.None)
+            {
+                p.Gameover("lose");
+                o.Gameover("win");
+            }
+            else
+            {
+                p.Gameover("draw");
+                o.Gameover("draw");
+            }
+
+            await Task.WhenAll(engine1.QuitAsync(), engine2.QuitAsync());
+        }
+
+        [TestMethod(), Timeout(5000)]
+        public async Task CancelGoTest()
+        {
+            var process = new RandomPlayer();
             using var engine = new UsiEngine(process);
 
-            // プロセス起動
             await engine.BeginAsync();
-            try
+            await Assert.ThrowsExceptionAsync<InvalidOperationException>(async () =>
             {
-                await engine.BeginAsync(); // 2回起動しようとしている
-                Assert.Fail();
-            }
-            catch (InvalidOperationException) { /* 成功 */ }
-
-            Assert.AreEqual("Mock0", engine.Name);
-            Assert.AreEqual("Author0", engine.Author);
-
-            // isready
+                await engine.BeginAsync();
+            });
             await engine.IsReadyAsync();
-
-            // usinewgame
             engine.StartNewGame();
 
-            // go
             var pos = new Position(Position.Hirate);
-            using var cts = new CancellationTokenSource(1000);
-
-            // 20秒の探索を1秒キャンセル
-            var (m1, p1) = await engine.GoAsync(pos, SearchLimit.Create(TimeSpan.FromSeconds(20.0)), cts.Token);
-
-            // ponder（違う局面を go : ponder ハズレ）
-            engine.GoPonder(pos, new SearchLimit());
-            pos.DoMove(m1);
-            await engine.GoAsync(pos, SearchLimit.Create(TimeSpan.FromMilliseconds(100.0)));
-
-            // ponder（キャンセル）
-            engine.GoPonder(pos, new SearchLimit());
-            await Task.Delay(100);
-            await engine.StopPonderAsync();
-
-            // ponder（同じ局面を go：ponderhit）
-            engine.GoPonder(pos, new SearchLimit());
-            await engine.GoAsync(pos, SearchLimit.Create(TimeSpan.FromMilliseconds(100.0)));
-
-            // gameover
-            engine.Gameover("win");
-
-            // quit
-            await engine.QuitAsync();
-
-            Assert.AreEqual(log.ToString(), 
-@"< usi
-  > id name Mock0
-  > id author Author0
-  > usiok
-< isready
-  > readyok
-< usinewgame
-< position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1 moves 
-< go btime 0 wtime 0 byoyomi 20000
-< stop
-  > bestmove 1g1f ponder 1c1d
-< position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1 moves 
-< go ponder btime 0 wtime 0 byoyomi 0
-< stop
-  > bestmove 1g1f ponder 1c1d
-< position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1 moves 1g1f
-< go btime 0 wtime 0 byoyomi 100
-  > bestmove 1c1d ponder 1f1e
-< position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1 moves 1g1f
-< go ponder btime 0 wtime 0 byoyomi 0
-< stop
-  > bestmove 1c1d ponder 1f1e
-< position sfen lnsgkgsnl/1r5b1/ppppppppp/9/9/9/PPPPPPPPP/1B5R1/LNSGKGSNL b - 1 moves 1g1f
-< go ponder btime 0 wtime 0 byoyomi 0
-< ponderhit
-  > bestmove 1c1d ponder 1f1e
-< gameover win
-< quit
-");
+            using var cts = new CancellationTokenSource(100);
+            await engine.GoAsync(pos, SearchLimit.Create(TimeSpan.FromSeconds(1000000.0)), cts.Token);
         }
 
         [TestMethod(), Timeout(5000)]
         public async Task GoAsyncTest()
         {
-            var (process, log) = CreateMockProcess();
+            var process = new RandomPlayer();
+            process.StdOutReceived += s => Trace.WriteLine($"> {s}");
 
             using var engine = new UsiEngine(process);
             await engine.BeginAsync();
@@ -114,16 +99,13 @@ namespace ShogiLibSharp.Engine.Tests
             // 1つ前の CancellationTokenSource のキャンセルに反応しないかテスト
             {
                 using var cts1 = new CancellationTokenSource();
-                var task0 = engine.GoAsync(pos, SearchLimit.Create(TimeSpan.Zero), cts1.Token);
-                await Task.Delay(1000);           // task0 が終わるぐらい十分な時間待つ
-                Assert.IsTrue(task0.IsCompleted); // 多分終わってる
+                await engine.GoAsync(pos, SearchLimit.Create(TimeSpan.FromSeconds(0.1)), cts1.Token);
                 using var cts2 = new CancellationTokenSource();
-                var task1 = engine.GoAsync(pos, SearchLimit.Create(TimeSpan.FromSeconds(1000000.0)), cts2.Token);
-                cts1.Cancel();         // 2回目の Go に対して、1つ目のキャンセルは無視
-                await Task.Delay(100); // 大体 100 ms ぐらい待てば、（キャンセルが効いているなら） bestmove が返ってきてるはず（しかし現在の実装では実際は bestmove を無限に待つ可能性もある → bestmove 待ちのタイムアウトが必要）
+                var task1 = engine.GoAsync(pos, SearchLimit.Create(TimeSpan.FromSeconds(10000.0)), cts2.Token);
+                cts1.Cancel();
+                await Task.Delay(100);
                 Assert.IsFalse(task1.IsCompleted);
                 cts2.Cancel();
-                await task0;
                 await task1;
             }
             // !task0.IsCompleted の間は 、次の Go はできない
@@ -385,99 +367,6 @@ namespace ShogiLibSharp.Engine.Tests
                     mock.Raise(x => x.Exited += null, new EventArgs());
                 });
             return mock.Object;
-        }
-
-        private static (IEngineProcess, StringBuilder) CreateMockProcess()
-        {
-            // 固定値と any の共存はできないっぽい
-            // mock.Setup(x => x.SendLine(It.IsAny<string>()))
-            // mock.Setup(x => x.SendLine("something"))
-
-            var mock = new Mock<IEngineProcess>();
-            var log = new StringBuilder();
-
-            Position? pos = null;
-            TaskCompletionSource? tcs = null;
-            mock.Setup(m => m.SendLine(It.IsAny<string>()))
-                .Callback(async (string s) =>
-                {
-                    log.AppendLine($"< {s}");
-                    if (s == "usi")
-                    {
-                        await Task.Delay(100);
-                        mock.Raise(x => x.StdOutReceived += null, "id name Mock0");
-                        mock.Raise(x => x.StdOutReceived += null, "id author Author0");
-                        mock.Raise(x => x.StdOutReceived += null, "usiok");
-                    }
-                    else if (s == "isready")
-                    {
-                        await Task.Delay(1000);
-                        mock.Raise(x => x.StdOutReceived += null, "readyok");
-                    }
-                    else if (s.StartsWith("position"))
-                    {
-                        var sfen = string.Join(' ', s.Split().Skip(2).Take(4));
-                        var moves = s
-                            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-                            .SkipWhile(x => x != "moves")
-                            .Skip(1)
-                            .Select(x => Usi.ParseMove(x));
-                        pos = new Position(sfen);
-                        foreach (var m in moves)
-                        {
-                            pos.DoMove(m);
-                        }
-                    }
-                    else if (s.StartsWith("go ponder"))
-                    {
-                        tcs = new TaskCompletionSource();
-                        await Task.WhenAny(Task.Delay(1000000000/* stop or ponderhit が来るまで待ちたい */), tcs.Task);
-                        var command = CreateBestmoveCommand(pos!);
-                        pos = null;
-                        mock.Raise(x => x.StdOutReceived += null, command);
-                    }
-                    else if (s.StartsWith("go"))
-                    {
-                        var byoyomi = int.Parse(s.Split()[6]);
-                        tcs = new TaskCompletionSource();
-                        await Task.WhenAny(Task.Delay(byoyomi), tcs.Task);
-                        var command = CreateBestmoveCommand(pos!);
-                        pos = null;
-                        mock.Raise(x => x.StdOutReceived += null, command);
-                    }
-                    else if (s == "stop")
-                    {
-                        tcs?.SetResult();
-                    }
-                    else if (s == "quit")
-                    {
-                        mock.Raise(x => x.Exited += null, null, new EventArgs());
-                    }
-                    else if (s == "ponderhit")
-                    {
-                        tcs?.SetResult();
-                    }
-                });
-
-            var obj = mock.Object;
-            obj.StdOutReceived += s =>
-            {
-                log.AppendLine($"  > {s}");
-            };
-
-            return (obj, log);
-        }
-
-        private static string CreateBestmoveCommand(Position pos)
-        {
-            var bestmove = Movegen.GenerateMoves(pos)[0];
-            pos.DoMove(bestmove);
-            var ponder = Movegen.GenerateMoves(pos).FirstOrDefault(Move.None);
-            var message = ponder != Move.None
-                ? $"bestmove {bestmove.Usi()} ponder {ponder.Usi()}"
-                : $"bestmove {bestmove.Usi()}";
-            pos.UndoMove();
-            return message;
         }
     }
 }
