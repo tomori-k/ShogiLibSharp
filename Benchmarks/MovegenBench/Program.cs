06e340ab42dc22d3b330cbb1c0290130625606ce
tomori-k <108829509+tomori-k@users.noreply.github.com>
2022-08-21 09:47:01 +0900

    MoveList クラスに書き換え、unsafe movegen 実装、ベンチマーク用プロジェクト作成 なぜか unsafe なし Unsafe のほうが速い（混乱） ベンチ結果： |             Method |    Mean |    Error |   StdDev | |------------------- |--------:|---------:|---------:| | PerftNoUnsafeBlock | 6.171 s | 0.2658 s | 0.0146 s | |     PerftUseUnsafe | 6.466 s | 1.3402 s | 0.0735 s |
    PerftNoUnsafeBlock: unsafe ブロックなし、Unsafe.As のみ使用
    PerftUseUnsafe: バッファのポインタ受け取り型、SIMD なし
    普通に考えると境界チェックとかがなくなる分ポインタ版の方が速くなるはず...？

---
+++ Benchmarks/MovegenBench/Program.cs
@@ -0,0 +1,81 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using ShogiLibSharp.Core;
+
+BenchmarkRunner.Run<Bench>();
+
+[IterationCount(3)]
+public class Bench
+{
+    static readonly (string, int, ulong)[] testcases = new[]
+    {
+        (Position.Hirate, 5, 19861490UL),
+        ("l6nl/5+P1gk/2np1S3/p1p4Pp/3P2Sp1/1PPb2P1P/P5GS1/R8/LN4bKL w RGgsn5p 1", 4, 516925165UL),
+        ("R8/2K1S1SSk/4B4/9/9/9/9/9/1L1L1L3 b RBGSNLP3g3n17p 1", 3, 53393368UL),
+    };
+
+    [Benchmark]
+    public void PerftNoUnsafeBlock()
+    {
+        
+        foreach (var (sfen, depth, expected) in testcases)
+        {
+            var pos = new Position(sfen);
+            var nodes = PerftImpl(pos, depth);
+            if (nodes != expected) throw new Exception($"sfen={sfen},nodes={nodes},expcted={expected}");
+        }
+    }
+
+    [Benchmark]
+    public unsafe void PerftUseUnsafe()
+    {
+        foreach (var (sfen, depth, expected) in testcases)
+        {
+            var pos = new Position(sfen);
+            var nodes = PerftUnsafeImpl(pos, depth);
+            if (nodes != expected) throw new Exception($"sfen={sfen},nodes={nodes},expcted={expected}");
+        }
+    }
+
+    private static ulong PerftImpl(Position pos, int depth)
+    {
+        var moves = Movegen.GenerateMoves(pos);
+
+        if (depth == 1)
+            return (ulong)moves.Count;
+
+        ulong count = 0;
+
+        foreach (Move m in moves)
+        {
+            pos.DoMove_PseudoLegal(m);
+            count += PerftImpl(pos, depth - 1);
+            pos.UndoMove();
+        }
+
+        return count;
+    }
+
+    private static unsafe ulong PerftUnsafeImpl(Position pos, int depth)
+    {
+        var moves = stackalloc Move[600];
+        var end = Movegen.GenerateMovesUnsafe(moves, pos);
+
+        if (depth == 1)
+            return (ulong)(end - moves);
+
+        ulong count = 0;
+        var p = moves;
+
+        while (p != end)
+        { 
+            pos.DoMove_PseudoLegal(*p++);
+            count += PerftImpl(pos, depth - 1);
+            pos.UndoMove();
+        }
+
+        return count;
+    }
+}
